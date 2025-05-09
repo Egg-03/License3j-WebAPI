084e090eedfc35b68fb09d8400e89bb2fc49896f
Egg-03 <111327101+Egg-03@users.noreply.github.com>
2025-05-09 20:24:39 +0530

    Add license saving functionality
    Introduced a new endpoint `/license/save` in LicenseController to save licenses to a file with a specified name and format. Updated LicenseService to include the `saveLicense` method, which validates the license state and writes it to a file using the specified IOFormat. Enhanced error handling with ResponseStatusException for better HTTP status code responses.

--- src/main/java/org/egg/license3j_spring/LicenseController.java
+++ src/main/java/org/egg/license3j_spring/LicenseController.java
@@ -1,9 +1,15 @@
 package org.egg.license3j_spring;
 
+import java.io.File;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import javax0.license3j.io.IOFormat;
 
 @RestController
 @RequestMapping("/api")
@@ -15,14 +21,34 @@ public class LicenseController {
 	 * 
 	 * @return HTTP:200 if license generation is successful, or else an HTTP:400 (Bad Request) if an unsaved license is in memory
 	 */
-	@GetMapping("/new")
+	@GetMapping("/license/new")
 	public ResponseEntity<String> generateNewLicense() {
 		try {
 			ls.newLicense();
 			return ResponseEntity.ok("A new license has been generated in memory");
-		} catch (IllegalStateException e) {
-			return ResponseEntity.badRequest().body("License in memory has not been saved. Please save the license first");
+		} catch (ResponseStatusException e) {
+			return ResponseEntity.status(e.getStatusCode()).body("License in memory has not been saved. Please save the license first");
 		}
 	}
+	
+	@GetMapping("/license/save")
+	public ResponseEntity<File> saveLicense(@RequestParam String licenseName, @RequestParam String format) {
+		
+		IOFormat ioformat;
+		switch(format) {
+		case "BINARY", "binary" -> ioformat=IOFormat.BINARY;
+		case "TEXT", "text" -> ioformat=IOFormat.STRING;
+		case "BASE64", "base64" -> ioformat=IOFormat.BASE64;
+		default -> ioformat=IOFormat.BINARY;
+		}
+		
+		try {
+			File f = ls.saveLicense(licenseName, ioformat);
+			return ResponseEntity.ok(f);
+		} catch (ResponseStatusException e) {
+			return ResponseEntity.status(e.getStatusCode()).body(null);	
+		}
+	}
+	// example usage: http://localhost:8080/api/license/save?licenseName=egg.bin&format=BINARY
 
 }
--- src/main/java/org/egg/license3j_spring/LicenseService.java
+++ src/main/java/org/egg/license3j_spring/LicenseService.java
@@ -1,10 +1,18 @@
 package org.egg.license3j_spring;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax0.license3j.License;
 import javax0.license3j.crypto.LicenseKeyPair;
+import javax0.license3j.io.IOFormat;
+import javax0.license3j.io.LicenseWriter;
 
 public class LicenseService {
 	
@@ -15,14 +23,38 @@ public class LicenseService {
 	private static final Logger logger = LoggerFactory.getLogger(LicenseService.class);
 
 	// generate a new license if there are no previously unsaved licenses
-		public void newLicense() {
+		public void newLicense() throws ResponseStatusException {
 			if (!licenseToSave) {
 				license = new License();
 				licenseToSign = true;
 				logger.info("A new license has been generated in memory"); 
 			} else {
 				logger.warn("An unsaved license is detected in memory");
-				throw new IllegalStateException("An unsaved license is detected in memory. Please save the license first.");
+				throw new ResponseStatusException(HttpStatus.CONFLICT, "An unsaved license is detected. Please save it first.");
+			}
+		}
+		
+		// save license to file
+		public File saveLicense(String licenseName, IOFormat format) throws ResponseStatusException {
+			if (license == null) {
+				logger.error("No license in memory. Please create or load a license");
+				throw new ResponseStatusException(HttpStatus.METHOD_NOT_ALLOWED, "No license in memory. Please create or load a license");
+			} 
+			
+			if(licenseToSign) {
+				logger.error("License needs to be signed before saving");
+				throw new ResponseStatusException(HttpStatus.METHOD_NOT_ALLOWED, "License needs to be signed before saving");
+			}
+			
+			File f = new File(licenseName);
+			try (LicenseWriter writer = new LicenseWriter(f)) {
+				writer.write(license, format);
+				licenseToSave = false;
+				logger.info("");
+				return f;
+			} catch (IOException e) {
+				logger.error(String.valueOf(e));
+				throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "An error occured during writing the license");
 			}
 		}
 
